3a2cedeb3533c7d7e7ec45d5c68a4f2ec88ed01c
Hossam Mostafa <hosammaarek@gmail.com>
2025-02-11 13:49:03 +0000

    Fixed Vite config with JSX support

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Ensure ESM syntax is used
 export default defineConfig({
   plugins: [react()],
-  root: "./src",  // Make sure Vite serves from `src`
-  build: {
-    outDir: "../dist",
-  },
   server: {
     port: 5173,
     open: true,
